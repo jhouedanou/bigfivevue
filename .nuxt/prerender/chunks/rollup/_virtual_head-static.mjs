0306b8dbac3253dd7c9f3750c5f99e7885661ad2
Jean Luc Houedanou <jeanluc@bigfiveabidjan.com>
2023-07-05 14:49:09 +0000

    bvhgcfxdw

--- .nuxt/prerender/chunks/rollup/_virtual_head-static.mjs
+++
@@ -1,4 +0,0 @@
-const _virtual__headStatic = {"headTags":"<meta charset=\"utf-8\">\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.20/fullpage.min.css\">\n<link rel=\"stylesheet\" href=\"https://use.typekit.net/lpb4lmp.css\">\n<script src=\"https://cdnjs.cloudflare.com/ajax/libs/masonry/4.0.0/masonry.pkgd.min.js\" type=\"text/javascript\"></script>\n<script src=\"https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.20/vendors/easings.min.js\" type=\"text/javascript\"></script>","bodyTags":"","bodyTagsOpen":"","htmlAttrs":"","bodyAttrs":""};
-
-export { _virtual__headStatic as default };
-//# sourceMappingURL=_virtual_head-static.mjs.map
