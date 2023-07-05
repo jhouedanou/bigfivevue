2cac4bb4e977050e73baa21cba6efd11a44b05ad
Jean Luc Houedanou <jeanluc@bigfiveabidjan.com>
2023-07-05 14:46:59 +0000

    :;,nbvcx

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,60 +1,62 @@
 export default defineNuxtConfig({
-  devtools: { enabled: true },
   target: 'static',
-  app: {
-    head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-      script: [
-        {
-          src: "https://cdnjs.cloudflare.com/ajax/libs/masonry/4.0.0/masonry.pkgd.min.js",
-          type: "text/javascript"
-        },
-        /*  {
-           src: "https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.20/fullpage.min.js",
-           type: "text/javascript"
-         },
-         {
-           src: "https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.20/fullpage.extensions.min.js",
-           type: "text/javascript"
-         }, */
-        {
-          src: "https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.20/vendors/easings.min.js",
-          type: "text/javascript"
-        },
-      ],
-      link: [
-        {
-          rel: 'stylesheet',
-          href: 'https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.20/fullpage.min.css'
-        },
-        {
-          rel: 'stylesheet',
-          href: 'https://use.typekit.net/lpb4lmp.css'
-        }/* ,
+  devtools: { enabled: true },
+  //modify nitro config to output in dist folder
+  nitro: {
+    app: {
+      head: {
+        charset: 'utf-8',
+        viewport: 'width=device-width, initial-scale=1',
+        script: [
+          {
+            src: "https://cdnjs.cloudflare.com/ajax/libs/masonry/4.0.0/masonry.pkgd.min.js",
+            type: "text/javascript"
+          },
+          /*  {
+             src: "https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.20/fullpage.min.js",
+             type: "text/javascript"
+           },
+           {
+             src: "https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.20/fullpage.extensions.min.js",
+             type: "text/javascript"
+           }, */
+          {
+            src: "https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.20/vendors/easings.min.js",
+            type: "text/javascript"
+          },
+        ],
+        link: [
+          {
+            rel: 'stylesheet',
+            href: 'https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.20/fullpage.min.css'
+          },
+          {
+            rel: 'stylesheet',
+            href: 'https://use.typekit.net/lpb4lmp.css'
+          }/* ,
         { src: '/fullpage.js/dist/fullpage.min.js' },
         { src: '/scrollmagic/scrollmagic/uncompressed/ScrollMagic.js' },
         { src: '/scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js' },
         { src: '/scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js' },
  */
-      ]
-    }
-  },
-  css: [
-    '@/assets/css/styles.scss',
-    'bootstrap/dist/css/bootstrap.min.css'
-  ],
-  plugins: [],
+        ]
+      }
+    },
+    css: [
+      '@/assets/css/styles.scss',
+      'bootstrap/dist/css/bootstrap.min.css'
+    ],
+    plugins: [],
 
-  components: true,
-  buildModules: [
-    '@nuxtjs/axios',
-  ]/* ,
+    components: true,
+    buildModules: [
+      '@nuxtjs/axios',
+    ]/* ,
   build: {
 
   } */
-  // Modules: https://go.nuxtjs.dev/config-modules
-  ,
-  modules: ['@bootstrap-vue-next/nuxt'
-  ],
-})
+    // Modules: https://go.nuxtjs.dev/config-modules
+    ,
+    modules: ['@bootstrap-vue-next/nuxt'
+    ],
+  })
