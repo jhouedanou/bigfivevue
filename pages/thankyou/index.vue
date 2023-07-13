ad39fbf8177a073245a8fd850829e7a9eb7be907
Jean Luc Houedanou <jeanluc@bigfiveabidjan.com>
2023-07-13 17:37:22 +0000

    thank you page

--- pages/thankyou/index.vue
+++ pages/thankyou/index.vue
@@ -1,7 +1,11 @@
 <script setup>
-
 </script>
 
-<template></template>
+<template>
+	<div>
+		<h1>Thank you</h1>
+		<p>Thank you for your message. We will get back to you as soon as possible.</p>
+	</div>
+</template>
 
 <style scoped></style>
