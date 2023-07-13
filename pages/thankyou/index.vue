d61f62c1224925eb612e12958b0352f7d4e0fa7f
Jean Luc Houedanou <jeanluc@bigfiveabidjan.com>
2023-07-13 17:40:39 +0000

    fel

--- pages/thankyou/index.vue
+++ pages/thankyou/index.vue
@@ -6,6 +6,7 @@
 		<h1>Thank you</h1>
 		<p>Thank you for your message. We will get back to you as soon as possible.</p>
 	</div>
+	<nuxt-link to="/">Retour à l'accueil</nuxt-link>
 </template>
 
 <style scoped></style>
