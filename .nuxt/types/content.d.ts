01b6003921084a689ae5692f9e73a238bafe3de5
Henri <sikefouet@gmail.com>
2023-11-06 12:07:35 +0000

    ok

--- .nuxt/types/content.d.ts
+++ .nuxt/types/content.d.ts
@@ -1,4 +1,4 @@
 declare module '#content/server' {
-  const serverQueryContent: typeof import('/Users/apple/Documents/bigfivevue/node_modules/@nuxt/content/dist/runtime/server').serverQueryContent
-  const parseContent: typeof import('/Users/apple/Documents/bigfivevue/node_modules/@nuxt/content/dist/runtime/server').parseContent
+  const serverQueryContent: typeof import('/Applications/MAMP/htdocs/bigfive/node_modules/@nuxt/content/dist/runtime/server').serverQueryContent
+  const parseContent: typeof import('/Applications/MAMP/htdocs/bigfive/node_modules/@nuxt/content/dist/runtime/server').parseContent
 }
