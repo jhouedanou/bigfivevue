3bb333240f08764d263a55a722aa6d975409a42e
Jean Luc Houedanou <jeanluc@bigfiveabidjan.com>
2024-02-19 17:22:11 +0000

    page individuelle solution

--- pages/solutions/[id].vue
+++ pages/solutions/[id].vue
@@ -1,6 +1,65 @@
+<script setup>
+import { ref, onMounted, reactive } from "vue";
+import axios from "axios";
+import PageLoader from "@/components/PageLoader.vue";
+let isSidebarOpen = ref(false);
+let toClose = ref(false);
+let closeBlack = ref(false);
+const solutions = ref([]);
+const state = reactive({
+  isLoading: true,
+});
+onMounted(async () => {
+  try {
+    state.isLoading = true;
+    //recupere la liste des solutions
+    const listeDesSolutions = await axios.get("api/solutions.json");
+    const data = listeDesSolutions.data;
+    solutions.value = data.map((solution) => ({
+      id: solution.id,
+      homepageVisuel: solution.homepageVisuel,
+      nomsolution: solution.nomsolution,
+      logo: solution.logo,
+      slug: solution.slug,
+    }));
+    state.isLoading = false;
+  } catch (error) {
+    console.log("Pas de solutions Big Five à afficher");
+  }
+});
+</script>
 <template>
-  <div id="singlesolutionwrapper">
-    <Header></Header>
-    <main>Liste des solutions</main>
-  </div>
+  <ContenuAltLayout>
+    <PageLoader v-if="state.isLoading" />
+    <div id="menumobile">
+      <Logo :id="3" v-if="!isSidebarOpen" />
+      <button
+        id="menutrigger"
+        @click="
+          isSidebarOpen = !isSidebarOpen;
+          isMainFull = !isMainFull;
+          toClose = !toClose;
+        "
+        :class="{ dana: closeBlack }"
+      >
+        <img v-if="toClose" src="/img/btn-close.svg" alt="" />
+        <img v-if="!toClose" src="/img/btn-menu.svg" alt="" />
+      </button>
+    </div>
+    <div
+      id="sidebar"
+      class="sidebar rakim"
+      :class="{ flipit: state.isSlide1Active, open: isSidebarOpen }"
+    >
+      <Logo :id="2" />
+      <Menu :page="'/agence'" />
+    </div>
+    <div
+      :id="`banner`"
+      :class="{ fixed: scrollPosition > 200, visible: scrollPosition > 200 }"
+    >
+      <img :src="matchingRealisation.banniere" alt="Image" class="img-fluid" />
+    </div>
+    <div id="singlesolutionwrapper"></div>
+  </ContenuAltLayout>
 </template>
--- pages/solutions/index.vue
+++ pages/solutions/index.vue
@@ -2,9 +2,18 @@
 import { ref, onMounted, reactive } from "vue";
 import axios from "axios";
 import SolutionsLayout from "@/layouts/SolutionsLayout.vue";
+import PageLoader from "@/components/PageLoader.vue";
+let isSidebarOpen = ref(false);
+let toClose = ref(false);
+let closeBlack = ref(false);
 const solutions = ref([]);
+const state = reactive({
+  isLoading: true,
+});
 onMounted(async () => {
   try {
+    state.isLoading = true;
+    //recupere la liste des solutions
     const listeDesSolutions = await axios.get("api/solutions.json");
     const data = listeDesSolutions.data;
     solutions.value = data.map((solution) => ({
@@ -14,6 +23,7 @@ onMounted(async () => {
       logo: solution.logo,
       slug: solution.slug,
     }));
+    state.isLoading = false;
   } catch (error) {
     console.log("Pas de solutions Big Five à afficher");
   }
@@ -23,6 +33,30 @@ onMounted(async () => {
 <template>
   <SolutionsLayout>
     <div id="listedessolutions">
+      <PageLoader v-if="state.isLoading" />
+      <div id="menumobile">
+        <Logo :id="3" v-if="!isSidebarOpen" />
+        <button
+          id="menutrigger"
+          @click="
+            isSidebarOpen = !isSidebarOpen;
+            isMainFull = !isMainFull;
+            toClose = !toClose;
+          "
+          :class="{ dana: closeBlack }"
+        >
+          <img v-if="toClose" src="/img/btn-close.svg" alt="" />
+          <img v-if="!toClose" src="/img/btn-menu.svg" alt="" />
+        </button>
+      </div>
+      <div
+        id="sidebar"
+        class="sidebar rakim"
+        :class="{ flipit: state.isSlide1Active, open: isSidebarOpen }"
+      >
+        <Logo :id="2" />
+        <Menu :page="'/agence'" />
+      </div>
       <ul class="d-flex flex-row">
         <li
           :style="{ backgroundImage: 'url(' + solution.homepageVisuel + ')' }"
