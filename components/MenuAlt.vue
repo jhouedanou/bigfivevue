20a4e6bbd1dc20d84f21c27ed9b01b9f30896132
Jean Luc Houedanou <jeanluc@bigfiveabidjan.com>
2024-02-29 10:28:40 +0000

    big formations

--- components/MenuAlt.vue
+++ components/MenuAlt.vue
@@ -1,26 +1,35 @@
 <script setup>
-import { reactive, onMounted } from 'vue';
-import axios from 'axios';
+import { reactive, onMounted } from "vue";
+import axios from "axios";
 const state = reactive({
-	menus: [],
+  menus: [],
+});
+const props = defineProps({
+  page: {
+    type: String,
+    required: true,
+  },
 });
 onMounted(async () => {
-	try {
-		const menuLinks = await axios.get('/api/menu.json');
-		state.menuLinks = menuLinks.data;
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    const menuLinks = await axios.get("/api/menu.json");
+    state.menuLinks = menuLinks.data;
+  } catch (error) {
+    console.log(error);
+  }
 });
 </script>
 <template>
-	<div class="menucontainer">
-		<ul id="menulinks">
-			<li v-for="link in state.menuLinks"
-				:key="link.id">
-				<nuxt-link :to="link.url">{{ link.nom }}</nuxt-link>
-			</li>
-		</ul>
-	</div>
+  <div class="menucontainer">
+    <ul id="menulinks">
+      <li
+        v-for="link in state.menuLinks"
+        :key="link.id"
+        :class="{ active: link.url === props.page }"
+      >
+        <nuxt-link :to="link.url">{{ link.nom }}</nuxt-link>
+      </li>
+    </ul>
+  </div>
 </template>
 <style lang="scss" scoped></style>
